41c7cbab8889afefac92d7ea8982c5ac9f65e058
cubeddu <juan.cubeddu@gmail.com>
2017-11-07 01:13:44 -0600

    removed dummy failing test

--- caltests/UnitTest1.cs
+++ caltests/UnitTest1.cs
@@ -12,12 +12,6 @@ namespace caltests
             Assert.Equal(6, Add(3, 3));
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
-
         int Add(int x, int y) => x + y;
     }
 }
